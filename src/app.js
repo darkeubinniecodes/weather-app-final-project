a615914314fa5bdbfdda5010242a2cbeb533f677
Catarina Santos <catarinasantos.info@gmail.com>
2023-02-01 16:26:49 +0000

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let dateElemenmt = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElemenmt.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "745a387cbd32a2ed51308fac14399b48";
