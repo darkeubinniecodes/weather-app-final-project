8fbeb2af3906b46ee1cd88bc2c11a60645349dc5
Catarina Santos <catarinasantos.info@gmail.com>
2023-02-04 15:44:06 +0000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "745a387cbd32a2ed51308fac14399b48";
-let city = "Leiria";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "745a387cbd32a2ed51308fac14399b48";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Leiria");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
